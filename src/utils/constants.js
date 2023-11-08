031f246cd0c3af41729a28d825240b4147501955
yoelvp <yoelvp73@gmail.com>
2023-11-07 23:19:03 -0500

    add: agregue llaves de identificacion para palabras clave

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LocalStorageKeys = {
+  CLIENT: 'client',
+  USER: 'user',
+  CART: 'cart',
+  AUTH: 'X-Auth-Creds'
+}
+
+export const Role = {
+  ADMIN: 'admin',
+  CLIENT: 'client'
+}
